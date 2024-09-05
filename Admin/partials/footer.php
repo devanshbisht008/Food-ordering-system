e3a8e2553b9c8dd256cef6fc9b22ac5cd2b9e94b
devanshbisht008 <144375390+devanshbisht008@users.noreply.github.com>
2024-09-05 12:37:06 +0530

    Add files via upload

---
+++ Admin/partials/footer.php
@@ -0,0 +1,8 @@
+<!-- Footer Section -->
+<div class="footer">
+    <div class="wrapper">
+        <p class="text-center">2024 All rights reserved to, Restaurant. Developed By-<a href="#"> Devansh Bisht</a></p>
+        </div>
+        
+    </div>
+    <!-- Footer Section -->
---
+++ Admin/partials/login-check.php
@@ -0,0 +1,11 @@
+<?php 
+    //authorization or access control
+   // if(!isset($_SESSION['user']))
+ //   {
+        //if user session is not set then user is not logged in
+    //    $_SESSION['no-login-message']="<div class='error text-center'>Please login to access admin panel.</div>";
+    //    header('location:'.SITEURL.'admin/login.php');
+  //  }
+    
+    
+?>
